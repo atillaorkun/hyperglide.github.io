80e6571f76a45ad2db10bed8a07dd7dc04c18f63
atillaorkun <59121650+atillaorkun@users.noreply.github.com>
2025-09-18 04:10:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// İletişim formu için basit bir uyarı
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Mesajınız gönderildi! Teşekkürler Hyperglide ekibi sizinle iletişime geçecek.");
+});
